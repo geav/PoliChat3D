94ee06383de8c8ead5216c7fa8c7654ef3ad3f35
Anderson Tavares <acmt@ecomp.poli.br>
2011-06-18 22:13:26 -0300

    Melhorando o Server e Connection
    Change-Id: I8e459ba84e9da0de5b2a294aba6ee241b53a969e
    Signed-off-by: Anderson Tavares <acmt@ecomp.poli.br>

---
+++ src/server/Connection.js
@@ -0,0 +1,15 @@
+function Connection(socket, connection_list)
+{
+	this.socket = socket;
+	this.connection_list = connection_list;
+	
+	this.notify_user_connected = function()
+	{
+	}
+	
+	this.notify_user_disconnected = function()
+	{
+	}
+	
+	
+}
--- src/server/Server.js
+++ src/server/Server.js
@@ -18,6 +18,7 @@ function Server(host, port)
 		function on_connect()
 		{
 			sys.puts('Usuario tentando se conectar');
+			
 			for(var i = 0; i < connection_list.length; i++)
 			{
 				if(connection_list[i].socket == socket)
@@ -26,55 +27,119 @@ function Server(host, port)
 					return;
 				}
 			}
-			var connection = new Connection(socket);
+			var connection = new Connection(socket, connection_list);
 			connection_list.push(connection);
+			
+			for(var i = 0; i < connection_list.length; i++)
+			{
+				connection_list[i].notify_user_connected(connection);
+			}
 			sys.puts('Usuario conectando');
 		}
 		
+		/**
+		 * If receiving policy request, send Policy XML
+		 * If receiving another data, treat it. 
+		 */
 		function on_data(data)
 		{
-			
+			if(String(data).indexOf('<policy-file-request/>') != -1)
+			{
+				sys.puts(policy());
+				socket.write(policy() + '\0');
+			}
+			else
+			{
+				for(var i = 0; i < connection_list.length; i++)
+				{
+					connection_list[i].process(data);
+				}
+			}
 		}
-		
+		/**
+		 * When the client finishes connection
+		 * 
+		 */
 		function on_end()
 		{
 			sys.puts('on_end');
 			disconnect();
 		}
-		
+		/**
+		 * Disconnect the client
+		 * 
+		 */
 		function disconnect()
 		{
+			var desconectado;
 			sys.puts('disconnect');
 			for(var i = 0; i < connection_list.length; i++)
 			{
 				if(connection_list[i].socket == socket)
 				{
-					socket_list.splice(i, 1);
+					desconectado = connection_list[i];
+					connection_list.splice(i, 1);
 					socket.end();
-					return;
+					connection_list
+					break;
 				}
+			}
+			for(var i = 0; i < connection_list.length; i++)
+			{
+				connection_list[i].notify_user_disconnected(desconectado);
 			}
 		}
 		
+		/**
+		 * When there is an error connecting 
+		 * 
+		 */
 		function on_error()
 		{
-			
+			console.log('on_error');
+			disconnect();
 		}
 		
+		/**
+		 * When the server tries to send and
+		 * has a timeout
+		 * 
+		 */
 		function on_timeout()
 		{
+			console.log('on_timeout');
+			disconnect();
+		}
+		
+		/**
+		 * Create Security Policy
+		 * 
+		 */
+		function policy()
+		{
+			var xml = '<?xml version="1.0"?>'+
+			          '<!DOCTYPE cross-domain-policy SYSTEM '+
+					  '"http://www.adobe.com/xml/dtds/cross-domain-policy.dtd">'+
+					  '<cross-domain-policy>'+
+					  '<allow-access-from domain="*" to-ports="7000"/>'+
+					  '</cross-domain-policy>';
 			
+			return xml;
 		}
 	});
 	
 	function start()
 	{
-		server.listen(host, port)
+		server.listen(host, port);
+		console.log('Server starting at address ' + host + ":" + port);
 	}
 	
 	function stop()
 	{
-		
+		while(connection_list.length > 0)
+		{
+			connection_list.pop().socket.end();
+		}
 	}
 }
 
