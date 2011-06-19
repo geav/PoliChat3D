ff2202b23aba7a8c5f5ecd7c83aa787c93c3b2c8
Anderson Tavares <nocturne.pe@gmail.com>
2011-06-19 02:42:16 -0300

    Criado o Modelo MVC no Cliente Criado o arquivo de conexão no cliente.
    Change-Id: I3ec9f1b04b30684cfcab1fbfced2a600a1ba1c83
    Signed-off-by: Anderson Tavares <nocturne.pe@gmail.com>

---
+++ src/client/controller/Connection.js
@@ -0,0 +1,40 @@
+function Connection()
+{
+	
+	this.enterRoom = function()
+	{
+		
+	}
+	
+	this.exitRoom = function()
+	{
+		
+	}
+	
+	this.sendMessage = function()
+	{
+		
+	}
+	
+	this.move = function()
+	{
+		
+	}
+	
+	this.connect = function()
+	{
+		
+	}
+	
+	this.disconnect = function()
+	{
+		
+	}
+	
+	this.listUsers = function()
+	{
+		
+	}
+	
+	
+}
