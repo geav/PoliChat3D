b7692fedad0ffe9b9cda2abe89503465df76c4b2
cleyton2304 <jcs@ecomp.poli.br>
2011-07-10 12:28:38 -0300

    personagem

--- src/client/personagem.js
+++ src/client/personagem.js
@@ -1,9 +1,8 @@
 
 function Personagem()
 {
-	this.cube = 0;
-	this.velX=0;
-	this.velZ=0;
+	this.cube;
+	this.direcao;
 	
 	this.inicializar = function()
 	{
@@ -15,34 +14,52 @@ function Personagem()
 		this.cube = new THREE.Mesh( new THREE.Cube( 100, 200, 100, 1, 1, 1, materials ), new THREE.MeshFaceMaterial() );
 		this.cube.position.y = 0;
 		this.cube.overdraw = true;
-	
+		this.direcao = new THREE.Vector3(0,0,0);
 	}
 	
 	this.atualizar = function()
 	{
-		this.cube.position.x-=this.velX;
-		this.cube.position.z-=this.velZ;
+		var VEL_PERSONAGEM = 5;
+		var direcaoFinal = new THREE.Vector3(0,0,0);
+				// movimentação lateral
+				
+		var angulo = this.cube.rotation.y + (Math.PI/180);
+		var ladoX = Math.cos(-angulo) * this.direcao.x;
+		var ladoZ = Math.sin(-angulo) * this.direcao.x;
+
+		direcaoFinal.x += ladoX * VEL_PERSONAGEM;
+		direcaoFinal.z += ladoZ * VEL_PERSONAGEM;
+				//////////////////
+
+		// movimentação frontal
+		angulo = this.cube.rotation.y - (90 * Math.PI/180);
+		var frenteX = Math.cos(-angulo) * this.direcao.z;
+		var frenteZ = Math.sin(-angulo) * this.direcao.z;
+
+		direcaoFinal.x += (frenteX * VEL_PERSONAGEM);
+		direcaoFinal.z += (frenteZ * VEL_PERSONAGEM);
+				
+		this.cube.position.x += direcaoFinal.x;
+		this.cube.position.z += direcaoFinal.z;
+		//////////////////////
+	}
+	
+	this.normalizeDirecao = function()
+	{
+		this.direcao.normalize();
 	}
 	
 	this.getCube = function(){
 		return this.cube;
 	}
 	
-	this.getVelX = function(){
-		return this.velX;
+	this.setDirecaoX = function(_direcao){
+		this.direcao.x = _direcao;
+	}
+	this.setDirecaoZ = function(_direcao){
+		this.direcao.z = _direcao;
 	}
 	
-	this.getVelZ = function(){
-		return this.velZ;
-	}
-	
-	 this.setVelX = function(parametro){
-		this.velX = parametro ;
-	}
-	
-	this.setVelZ = function(parametro){
-		this.velZ = parametro;
-	}
 	
 	
 }
